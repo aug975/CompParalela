e03aac08dd855953d76db18a7ded8ad5281c6050
Augusto Rassi Scrideli <101229028+aug975@users.noreply.github.com>
2022-10-28 22:29:09 -0300

    codigo de trapezio

---
+++ lab07/main.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+#include <math.h>
+
+//declaracao da funcao f (exp) utilizada em Trap()
+double f(double x){
+        return exp(x);
+}
+
+//declaracao de Trap
+void Trap (double a, double b, int n, double *global_result_p);
+
+//main do codigo, pede input do user e roda Trap com base nestes dados
+//retorna resultados
+int main (int argc, char * argv []) {
+        double global_result = 0.0;
+        double a, b;
+        int n;
+        int thread_count;
+        thread_count = strtol ( argv [1] , NULL , 10);
+        printf (" Enter a, b and n\n");
+        scanf ("%lf %lf %d", &a, &b, &n);
+        #pragma omp parallel num_threads (thread_count)
+        Trap (a, b, n, &global_result);
+
+        printf ("With n = %d trapezoids , our estimate \n", n);
+        printf ("of the integral from %g to %g = %.14e\n", a, b, global_result+1);
+        return 0;
+}
+
+//funcao trap que faz os calculos
+void Trap (double a, double b, int n, double *global_result_p){
+        double h, x, my_result;
+        double local_a, local_b;
+        int i, local_n;
+        int my_rank = omp_get_thread_num();
+        int thread_count = omp_get_num_threads();
+        h = (b-a)/n;
+        local_n = n/thread_count;
+        local_a = a + my_rank * local_n *h;
+        local_b = local_a + local_n * h;
+        my_result = (f(local_a) + f(local_b))/2.0;
+        for (i=1; i < local_n; i++) {
+                x = local_a + i*h;
+                my_result += f(x);
+        }
+        my_result = my_result*h;
+        #pragma omp critical
+        *global_result_p += my_result;
+}
