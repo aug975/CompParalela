9f5c2d19fab05d6f482c41c2325afbed6eb1d2bb
Augusto Rassi Scrideli <101229028+aug975@users.noreply.github.com>
2022-11-06 23:47:11 -0300

    Create multipmatriz_omp_critical.c

---
+++ lab07/multipmatriz_omp_critical.c
@@ -0,0 +1,60 @@
+
+#include <omp.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+int N = 2;
+int M = 2;
+int P = 2;
+
+void mostrarMatriz(int matriz[],int tam1,int tam2){
+  int i,j;
+  for (i = 0; i < tam1; i++){
+    for (j = 0; j < tam2; j++){
+      printf("%d ", matriz[i*tam2+j]);}
+    printf("\n");
+  }
+}
+
+int main(int argc , char * argv []){
+  srand(time(NULL));
+  int thread_count;
+  thread_count = strtol ( argv [1] , NULL , 10);
+  unsigned long i, j, k;
+  int A[N][P];
+  int B[P][M];
+  int matres[N][M];
+
+  for (i = 0;i < N; i++)
+    for (k = 0; k < P; k++)
+      A[i][k] = rand()%100;
+  for (k = 0;k < P; k++)
+    for (j = 0; j < M; j++)
+      B[k][j] = rand()%100;
+  for (i = 0;i < N; i++)
+    for (j = 0; j < M; j++)
+      matres[i][j] = 0;
+
+  
+  #pragma omp shared(A,B,matres) private(i,j,k)
+  #pragma omp parallel num_threads(thread_count)
+  {
+    for (i = 0; i < N; i++) {
+      for (j = 0; j < M; j++) {
+        for (k = 0; k < P; k++) {
+          #pragma omp critical
+          matres[i][j] += A[i][k] * B[k][j];
+        }
+      }
+    }
+    
+  }
+  printf("Matriz A:\n");
+  mostrarMatriz(*A,N,P);
+  printf("Matriz B:\n");
+  mostrarMatriz(*B,P,M);
+  printf("Matriz resultado:\n");
+  mostrarMatriz(*matres,N,M);
+  return 0;
+}
